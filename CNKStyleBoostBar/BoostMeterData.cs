50bf3c12ef84c23aed8cd727233dc99bd7a056ef
Emkayex <53287857+Emkayex@users.noreply.github.com>
2024-05-01 21:17:31 -0500

    Started Work on BoostMeterData and CNKStyleBoostMeter Classes

---
+++ CNKStyleBoostBar/BoostMeterData.cs
@@ -0,0 +1,197 @@
+using GameOverlay.Drawing;
+
+using GetMeterColorFuncType = System.Func<float, float, float, GameOverlay.Drawing.SolidBrush>;
+
+namespace CNKStyleBoostBar;
+class BoostMeterData
+{
+    public readonly float[] BoostAmounts = new float[3];
+    public float DrawX { get; set; }
+    public float DrawY { get; set; }
+    public float ViewportWidth { get; set; }
+    public float ViewportHeight { get; set; }
+    public float MinValueForBoost { get; set; }
+    public float MaxValueForBoost { get; set; }
+    public float ArcStartAngle { get; set; } = 45f;
+    public float ArcEndAngle { get; set; } = -30f;
+    public GetMeterColorFuncType GetMeterColorForFillPercent;
+    public float DriftDirection { get; set; } = 1f;
+    public DateTimeOffset LastUpdate { get; private set; } = DateTimeOffset.UtcNow;
+
+    private readonly BrushCollection Brushes;
+
+    public BoostMeterData(GetMeterColorFuncType getMeterColorForFillPercent, BrushCollection brushes)
+    {
+        GetMeterColorForFillPercent = getMeterColorForFillPercent;
+        Brushes = brushes;
+    }
+
+    public (int?, float?) GetBoostNumberAndValue()
+    {
+        for (var i = BoostAmounts.Length - 1; i >= 0; i--)
+        {
+            var boostAmount = BoostAmounts[i];
+            if (boostAmount > 0)
+            {
+                return (i, boostAmount);
+            }
+        }
+
+        return (null, null);
+    }
+
+    public void DrawBoostBar(Graphics gfx, BoostBarStyle style, int boostNum, float baseX, float baseY)
+    {
+        // Get the boost value to use for the meter
+        var boostValue = BoostAmounts[boostNum];
+
+        if (style == BoostBarStyle.Rectangle)
+        {
+            DrawRectangleBoostBars(gfx, boostValue, boostNum, baseX, baseY);
+        }
+        else if ((style == BoostBarStyle.ArcsSameAngles) || (style == BoostBarStyle.ArcsSameLength))
+        {
+            DrawArcBoostBars(gfx, style, boostValue, boostNum, baseX, baseY);
+        }
+    }
+
+    private SolidBrush GetMeterBrushColor(float boostValue) => GetMeterColorForFillPercent(boostValue, MinValueForBoost, MaxValueForBoost);
+
+    private void DrawRectangleBoostBars(Graphics gfx, float boostValue, int boostNum, float baseX, float baseY)
+    {
+        const float BaseBoostBarWidth = 20f;
+        const float BaseBoostBarHeight = 100f;
+        const float BaseSpacingBetweenBars = 10f;
+        const float BaseSpaceBetweenStartOfBars = BaseBoostBarWidth + BaseSpacingBetweenBars;
+
+        // Calculate the boost bar width, height, and spacing based on the needed UI element scale for different display sizes and multiplayer
+        var uiScale = CNKStyleBoostMeter.DisplayInfo.GetUIScaleY(ViewportHeight);
+        var boostBarWidth = BaseBoostBarWidth * uiScale;
+        var boostBarHeight = BaseBoostBarHeight * uiScale;
+        var spaceBetweenStartOfBars = BaseSpaceBetweenStartOfBars * uiScale;
+
+        // Calculate a local X for this boost bar where the middle bar should be centered
+        // The positioning in the Y-direction doesn't need to change depending on the direction of the drift
+        var centeredBoostNum = (BoostAmounts.Length - 1) / 2f;
+        var boostOffsetFromCenter = boostNum - centeredBoostNum;
+        var rectLocalX = (boostBarWidth / -2f) + (boostOffsetFromCenter * spaceBetweenStartOfBars);
+        if (DriftDirection < 0)
+        {
+            // Flip the direction when drifting in the negative X-direction to fill the bars in "opposite" order
+            rectLocalX *= -1;
+            rectLocalX -= boostBarWidth;
+        }
+
+        // Calculate an offset from the player's base point in the X and Y directions
+        var xOffset = ViewportWidth / 10f * DriftDirection;
+        var yOffset = -2f * boostBarHeight;
+
+        // Calculate the X and Y positions at which the bar should be drawn
+        var x = baseX + rectLocalX + xOffset;
+        var y = baseY + yOffset;
+
+        // Draw the boost bar (with a gray background)
+        var rect = Rectangle.Create(x, y, boostBarWidth, boostBarHeight);
+        gfx.FillRectangle(Brushes.Gray, rect);
+        gfx.DrawHorizontalProgressBar(Brushes.Black, GetMeterBrushColor(boostValue), rect, 3f * uiScale, boostValue / MaxValueForBoost * 100f);
+    }
+
+    private void DrawArcBoostBars(Graphics gfx, BoostBarStyle style, float boostValue, int boostNum, float baseX, float baseY)
+    {
+        const float baseInnerRadius = 75f - 10f;
+        const float baseOuterRadius = 95f - 10f;
+
+        // Calculate some base values based off the config options
+        var baseStartAngle = ArcStartAngle;
+        var baseEndAngle = ArcEndAngle;
+        var baseAngleDelta = baseEndAngle - baseStartAngle;
+        var baseOuterArcProduct = baseOuterRadius * baseAngleDelta;
+
+        // Get a scaling factor for the UI elements
+        var uiScale = CNKStyleBoostMeter.DisplayInfo.GetUIScaleY(ViewportHeight);
+
+        // Calculate the radii for the arc based on the boost number
+        var extraRadius = boostNum * (30f * uiScale);
+        var innerRadius = (baseInnerRadius * uiScale) + extraRadius;
+        var outerRadius = (baseOuterRadius * uiScale) + extraRadius;
+
+        // Calculate the start and end angle based on the direction being faced
+        var (startAngle, endAngle) = (baseStartAngle, baseEndAngle);
+        if (DriftDirection < 0)
+        {
+            startAngle = 180f - startAngle;
+            endAngle = 180f - endAngle;
+        }
+
+        // Calculate the angle delta necessary to make this arc length equal to the base arc length
+        if (style == BoostBarStyle.ArcsSameLength)
+        {
+            var neededAngleDelta = baseOuterArcProduct / outerRadius;
+            var amountToSubtractOffStartAndEnd = (baseAngleDelta - neededAngleDelta) / 2f;
+            startAngle += amountToSubtractOffStartAndEnd * DriftDirection;
+            endAngle -= amountToSubtractOffStartAndEnd * DriftDirection;
+        }
+        var angleDelta = endAngle - startAngle;
+
+        // Calculate the center of the circle from which the arc originates
+        var xOffset = ViewportWidth / 25f * DriftDirection;
+        var yOffset = ViewportHeight / 5f;
+        var center = new Point(baseX + xOffset, baseY - yOffset);
+
+        // Draw the boost bar as an arc
+        var fullArcGeo = CreateArcDegrees(gfx, center, innerRadius, outerRadius, startAngle, endAngle);
+        var fillArcGeo = CreateArcDegrees(gfx, center, innerRadius, outerRadius, startAngle, startAngle + (angleDelta * boostValue / MaxValueForBoost));
+        gfx.FillGeometry(fullArcGeo, Brushes.Gray);
+        gfx.FillGeometry(fillArcGeo, GetMeterBrushColor(boostValue));
+        gfx.DrawGeometry(fullArcGeo, Brushes.Black, 3f * uiScale);
+    }
+
+    private static Geometry CreateArcDegrees(Graphics gfx, Point center, float innerRadius, float outerRadius, float startAngle, float endAngle)
+    {
+        return CreateArcRadians(
+            gfx: gfx,
+            center: center,
+            innerRadius: innerRadius,
+            outerRadius: outerRadius,
+            startAngle: startAngle * MathF.PI / 180f,
+            endAngle: endAngle * MathF.PI / 180f
+        );
+    }
+
+    private static Geometry CreateArcRadians(Graphics gfx, Point center, float innerRadius, float outerRadius, float startAngle, float endAngle)
+    {
+        static Point RadiusAndAngleToPoint(float radius, float angle) => new(radius * MathF.Cos(angle * -1f), radius * MathF.Sin(angle * -1f));
+
+        static Point AddPoints(Point pt1, Point pt2) => new(pt1.X + pt2.X, pt1.Y + pt2.Y);
+
+        // Calculate the four points used to draw the arc by converting from polar coordinates to cartesian coordinates
+        var pointsNoOffset = new Point[]
+        {
+            RadiusAndAngleToPoint(innerRadius, startAngle),
+            RadiusAndAngleToPoint(innerRadius, endAngle),
+            RadiusAndAngleToPoint(outerRadius, endAngle),
+            RadiusAndAngleToPoint(outerRadius, startAngle)
+        };
+
+        // Add the points to the circle center to offset them correctly
+        var points = pointsNoOffset.Select(pt => AddPoints(center, pt)).ToArray();
+
+        var (drawInnerRadius, drawOuterRadius) = (innerRadius, outerRadius * -1f);
+        if (endAngle < startAngle)
+        {
+            drawInnerRadius *= -1f;
+            drawOuterRadius *= -1f;
+        }
+
+        // Create and return the Geometry object
+        var geo = gfx.CreateGeometry();
+        geo.BeginFigure(points[0], true);
+        geo.AddCurve(points[1], drawInnerRadius);
+        geo.AddPoint(points[2]);
+        geo.AddCurve(points[3], drawOuterRadius);
+        geo.AddPoint(points[0]);
+        geo.EndFigure();
+        geo.Close();
+        return geo;
+    }
+}
---
+++ CNKStyleBoostBar/CNKStyleBoostMeter.cs
@@ -0,0 +1,183 @@
+using GameOverlay.Drawing;
+using GameOverlay.Windows;
+
+namespace CNKStyleBoostBar;
+public class CNKStyleBoostMeter
+{
+    public DisplayInformation DisplayInfo;
+
+    public BoostBarStyle ConfigBoostBarStyle { get; set; } = BoostBarStyle.ArcsSameAngles;
+    public string BoostMeterColor1 { get; set; } = "00FF00";
+    public string BoostMeterColor2 { get; set; } = "FFD800";
+    public string BoostMeterColor3 { get; set; } = "FF6A00";
+    public string BoostMeterColor4 { get; set; } = "FF0000";
+
+    public float ArcStyleStartAngle { get; set; } = -30f;
+    public float ArcStyleEndAngle { get; set; } = 45f;
+    public float ThresholdPercentForColor3 { get; set; } = 0.80f;
+    public float ThresholdPercentForColor4 { get; set; } = -0.95f;
+
+    private readonly HashSet<int> CustomBrushCreationRequests = [];
+    private readonly Dictionary<int, SolidBrush> CustomBrushes = [];
+
+    private readonly GraphicsWindow Window;
+    private BrushCollection? Brushes = null;
+
+    private readonly BoostMeterData MeterData;
+
+    public CNKStyleBoostMeter()
+    {
+        // Create the DisplayInformation object with placeholder values that will be overwritten once the first frame is captured
+        DisplayInfo = new()
+        {
+            SystemWidth = 1920,
+            SystemHeight = 1080,
+            RenderWidth = 1920,
+            RenderHeight = 1080
+        };
+
+        // Configure the overlay classes
+        var gfx = new Graphics
+        {
+            MeasureFPS = false,
+            PerPrimitiveAntiAliasing = false,
+            TextAntiAliasing = false
+        };
+
+        Window = new GraphicsWindow(0, 0, DisplayInfo.SystemWidth, DisplayInfo.SystemHeight)
+        {
+            FPS = 60,
+            IsTopmost = true,
+            IsVisible = true
+        };
+
+        // Set the event callbacks
+        #pragma warning disable CS8622
+        Window.SetupGraphics += Window_SetupGraphics;
+        Window.DrawGraphics += Window_DrawGraphics;
+        Window.DestroyGraphics += Window_DestroyGraphics;
+        #pragma warning restore CS8622
+
+        // Start the overlay
+        Task.Run(StartOverlay);
+    }
+
+    private static SolidBrush GetBoostMeterColor(float boostBarValue, float minValueForBoost, float maxValueForBoost)
+    {
+        // Get the hex code for the brush color
+        var brushHex = BoostMeterColor1.Value;
+        if (boostBarValue >= minValueForBoost)
+        {
+            brushHex = BoostMeterColor2.Value;
+        }
+        if (boostBarValue >= (maxValueForBoost * ThresholdPercentForColor3.Value))
+        {
+            brushHex = BoostMeterColor3.Value;
+        }
+        if (boostBarValue >= (maxValueForBoost * ThresholdPercentForColor4.Value))
+        {
+            brushHex = BoostMeterColor4.Value;
+        }
+
+        // Convert the hex code to an integer to perform a lookup
+        var brushInt = Convert.ToInt32(brushHex, 16);
+        if (!CustomBrushes.TryGetValue(brushInt, out var brush))
+        {
+            // If the value isn't present in the dictionary, submit a request to overlay render loop to create a new brush
+            // Attempting to use a Graphics object from here will crash the game
+            lock (CustomBrushCreationRequests)
+            {
+                CustomBrushCreationRequests.Add(brushInt);
+            }
+
+            // Since the necessary brush isn't available yet, just return a default black brush
+            brush = Brushes!.Black;
+        }
+
+        return brush;
+    }
+
+    public static void StartOverlay()
+    {
+        Window.Create();
+        Window.Join();
+    }
+
+    public static void StopOverlay()
+    {
+        Window.Dispose();
+        Window.Join();
+    }
+
+    private static void Window_SetupGraphics(object sender, SetupGraphicsEventArgs e)
+    {
+        // Configure the collections used to draw on the screen
+        var gfx = e.Graphics;
+        if (e.RecreateResources)
+        {
+            Brushes?.Dispose();
+        }
+
+        Brushes ??= new BrushCollection(gfx);
+    }
+
+    private static void Window_DrawGraphics(object sender, DrawGraphicsEventArgs e)
+    {
+        // Clear the screen before drawing any graphics
+        var gfx = e.Graphics;
+        gfx.ClearScene();
+
+        // Handle any brush creation requests
+        if (CustomBrushCreationRequests.Count > 0)
+        {
+            lock (CustomBrushCreationRequests)
+            {
+                foreach (var brushInt in CustomBrushCreationRequests)
+                {
+                    // Parse the RGB values from the brush integer
+                    var r = (brushInt >> (8 * 2)) & 0xFF;
+                    var g = (brushInt >> 8) & 0xFF;
+                    var b = brushInt & 0xFF;
+
+                    var newBrush = gfx.CreateSolidBrush(r, g, b);
+                    CustomBrushes.TryAdd(brushInt, newBrush);
+                }
+
+                // Clear out the brush creation requests set
+                CustomBrushCreationRequests.Clear();
+            }
+        }
+
+        // Only render the text if the brushes and fonts are available
+        if (Brushes is not null)
+        {
+            // foreach (var (_, meterData) in PlayerBoostMeterData)
+            // {
+            //     gfx.DrawCrosshair(Brushes.Blue, new(meterData.DrawX, meterData.DrawY), 25f, 5f, CrosshairStyle.Plus);
+            // }
+
+            // For each player, draw the current boost amount on the screen
+            foreach (var (playerNum, boostMeterData) in PlayerBoostMeterData)
+            {
+                if (boostMeterData.LastUpdate.AddSeconds(0.5) < DateTimeOffset.Now)
+                {
+                    continue;
+                }
+
+                var (boostNum, boostValue) = boostMeterData.GetBoostNumberAndValue();
+                if ((boostNum is not null) && (boostValue is not null))
+                {
+                    for (var drawBoostNum = 0; drawBoostNum <= 2; drawBoostNum++)
+                    {
+                        boostMeterData.DrawBoostBar(gfx, ConfigBoostBarStyle, drawBoostNum, boostMeterData.DrawX, boostMeterData.DrawY);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void Window_DestroyGraphics(object sender, DestroyGraphicsEventArgs e)
+    {
+        Brushes?.Dispose();
+    }
+}
