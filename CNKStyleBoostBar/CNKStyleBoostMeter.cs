cc07a0d529e99ce916578af0d612cefd6c344826
Emkayex <53287857+Emkayex@users.noreply.github.com>
2024-05-01 21:34:52 -0500

    Brought in Code from Program.cs Test

--- CNKStyleBoostBar/CNKStyleBoostMeter.cs
+++ CNKStyleBoostBar/CNKStyleBoostMeter.cs
@@ -1,11 +1,19 @@
 using GameOverlay.Drawing;
 using GameOverlay.Windows;
+using WindowCapture;
+using WindowCapture.window_capture;
+
+using PixelColor = (byte r, byte g, byte b, byte a);
 
 namespace CNKStyleBoostBar;
 public class CNKStyleBoostMeter
 {
     public const float MinValueForBoost = 0.5f;
     public const float MaxValueForBoost = 1.0f;
+
+    private static readonly PixelColor GrayTuple = (0x5F, 0x5E, 0x5F, 0xFF);
+    private static readonly PixelColor RedTuple = (0xF5, 0x00, 0x00, 0xFF);
+    private static readonly double[] AreaSamplePercentages = [0.1, 0.5, 0.9];
 
     public readonly DisplayInformation DisplayInfo;
 
@@ -26,6 +34,7 @@ public class CNKStyleBoostMeter
     private readonly GraphicsWindow Window;
     private BrushCollection? Brushes = null;
 
+    private readonly FrameCapture Capturer;
     private readonly BoostMeterData MeterData;
 
     public CNKStyleBoostMeter()
@@ -64,9 +73,102 @@ public class CNKStyleBoostMeter
         // Create the object used to track boost meter data
         MeterData = new(GetBoostMeterColor, () => Brushes, () => DisplayInfo);
 
+        // Start the frame capturer that also records current boost values
+        Capturer = new();
+        Capturer.FrameReady += OnFrameCaptured;
+        Capturer.StartCapture("TheKarters2");
+
         // Start the overlay
         Task.Run(StartOverlay);
     }
+
+    private void OnFrameCaptured(object? sender, FrameCapturedEventArgs e)
+    {
+        // Use only the lower portion of the screen from 75% width and height to the edges to reduce the number of pixels to process
+        var areaLeftBound = (int)(e.Width * 0.75);
+        var areaTopBound = (int)(e.Height * 0.75);
+
+        // Iterate through all of the pixels and find the boundaries of gray and red pixels from the boost bars
+        // The values are stored in the array as R, G, B, A, R, G, B, A, ...
+        // Therefore, pixels must be processed as groups of four bytes
+        var leftMost = (int)e.Width;
+        var rightMost = areaLeftBound;
+        var topMost = (int)e.Height;
+        var bottomMost = areaTopBound;
+        for (var xRaw = areaLeftBound; xRaw < e.Width; xRaw++)
+        {
+            for (var yRaw = areaTopBound; yRaw < e.Height; yRaw++)
+            {
+                // For the given pixel identified by (xRaw, yRaw), calculate the array index if all RGBA values were stored as 32-bit uints
+                // Then multiply the index by four to get the correct index for the R byte
+                var indexRaw = (yRaw * e.Width) + xRaw;
+                var rIndex = (int)(indexRaw * 4);
+                var r = e.RgbaValues[rIndex];
+                var g = e.RgbaValues[rIndex + 1];
+                var b = e.RgbaValues[rIndex + 2];
+                var a = e.RgbaValues[rIndex + 3];
+                if (IsGray(r, g, b, a) || IsRed(r, g, b, a))
+                {
+                    leftMost = Math.Min(xRaw, leftMost);
+                    rightMost = Math.Max(xRaw, rightMost);
+                    topMost = Math.Min(yRaw, topMost);
+                    bottomMost = Math.Max(yRaw, bottomMost);
+                }
+            }
+        }
+
+        // If the selected area width or height is not positive, clear the latest boost values and then return
+        var width = rightMost - leftMost;
+        var height = bottomMost - topMost;
+        if ((width <= 0) || (height <= 0))
+        {
+            for (var i = 0; i < MeterData.BoostAmounts.Length; i++)
+            {
+                MeterData.BoostAmounts[i] = 0;
+            }
+            return;
+        }
+
+        // The bars can be sampled at the 10%, 50%, and 90% X-positions within the selected area to count the red and gray pixels
+        // A black bar is missing where the fill bar cuts off, but that will be counted as a red pixel instead of a gray pixel
+        for (var i = 0; i < MeterData.BoostAmounts.Length; i++)
+        {
+            var percent = AreaSamplePercentages[i];
+            var xRaw = (int)(leftMost + (width * percent));
+
+            var grayCount = 0;
+            var redCount = 0;
+            for (var yRaw = topMost; yRaw < topMost + height; yRaw++)
+            {
+                // Calculate the index of the red byte for an RGBA value
+                var indexRaw = (yRaw * e.Width) + xRaw;
+                var rIndex = (int)(indexRaw * 4);
+
+                // Retrieve the RGBA values and check if the pixel is gray
+                // Anything other than gray will be counted as red since those should be the only two colors present besides the black bar
+                var r = e.RgbaValues[rIndex];
+                var g = e.RgbaValues[rIndex + 1];
+                var b = e.RgbaValues[rIndex + 2];
+                var a = e.RgbaValues[rIndex + 3];
+                if (IsGray(r, g, b, a))
+                {
+                    grayCount++;
+                }
+                else
+                {
+                    redCount++;
+                }
+            }
+
+            // Calculate the portion that's filled and store it to the appropriate array position
+            var portionFilled = 1.0f * redCount / (redCount + grayCount);
+            MeterData.BoostAmounts[i] = portionFilled;
+        }
+    }
+
+    private static bool IsGray(byte r, byte g, byte b, byte a) => IsPixelColor(r, g, b, a, GrayTuple);
+    private static bool IsRed(byte r, byte g, byte b, byte a) => IsPixelColor(r, g, b, a, RedTuple);
+    private static bool IsPixelColor(byte r, byte g, byte b, byte a, PixelColor color) => (r == color.r) && (g == color.g) && (b == color.b) && (a == color.a);
 
     private SolidBrush GetBoostMeterColor(float boostBarValue)
     {
@@ -177,5 +279,6 @@ public class CNKStyleBoostMeter
     private void Window_DestroyGraphics(object sender, DestroyGraphicsEventArgs e)
     {
         Brushes?.Dispose();
+        Capturer.Dispose();
     }
 }
