60e4cdbbd6ef7e2fac3ee67126abb6f20ac5b645
Emkayex <53287857+Emkayex@users.noreply.github.com>
2024-05-16 21:34:06 -0500

    Initial Changes to Support CLI Arguments

--- CNKStyleBoostBar/Program.cs
+++ CNKStyleBoostBar/Program.cs
@@ -1,7 +1,18 @@
-﻿namespace CNKStyleBoostBar;
+﻿using DotMake.CommandLine;
+
+namespace CNKStyleBoostBar;
 internal class Program
 {
     private static void Main(string[] args)
+    {
+        Cli.Run<RootCommand>(args);
+    }
+}
+
+[CliCommand]
+internal class RootCommand
+{
+    public void Run()
     {
         var meterApp = new CNKStyleBoostMeter();
         meterApp.StartCaptureAndOverlay();
