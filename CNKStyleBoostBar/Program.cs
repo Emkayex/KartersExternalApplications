cbd4102d9ebf9a2e42a8c68923048f9ec500052d
Emkayex <53287857+Emkayex@users.noreply.github.com>
2024-05-17 22:01:34 -0500

    Added Options to Configure the Boost Bar Colors

--- CNKStyleBoostBar/Program.cs
+++ CNKStyleBoostBar/Program.cs
@@ -18,16 +18,35 @@ internal class RootCommand
     [CliOption(Description = "Draws a debug box around the original boost meter.")]
     public bool DrawDebugBox { get; set; } = false;
 
-    [CliOption]
+    [CliOption(Description = "Changes the shape of the boost meters drawn on screen.")]
     public BoostBarStyle BoostBarStyle { get; set; } = BoostBarStyle.ArcsSameAngles;
+
+    [CliOption(Description = "The boost meter color when the minimum amount for a boost hasn't been reached yet (RGB hex).")]
+    public string BoostMeterColor1 { get; set; } = "00FF00";
+
+    [CliOption(Description = "The boost meter color when the minimum amount for a boost has been reached (RGB hex).")]
+    public string BoostMeterColor2 { get; set; } = "FFD800";
+
+    [CliOption(Description = "The boost meter color when the first threshold within the valid boost window has been reached (RGB hex).")]
+    public string BoostMeterColor3 { get; set; } = "FF6A00";
+
+    [CliOption(Description = "The boost meter color when the second threshold within the valid boost window has been reached (RGB hex).")]
+    public string BoostMeterColor4 { get; set; } = "FF0000";
 
     public void Run()
     {
+        // Create and configure the application
         var meterApp = new CNKStyleBoostMeter(MirrorMeter)
         {
             DrawDebugBox = DrawDebugBox,
-            ConfigBoostBarStyle = BoostBarStyle
+            ConfigBoostBarStyle = BoostBarStyle,
+            BoostMeterColor1 = BoostMeterColor1,
+            BoostMeterColor2 = BoostMeterColor2,
+            BoostMeterColor3 = BoostMeterColor3,
+            BoostMeterColor4 = BoostMeterColor4
         };
+
+        // Run the overlay application until the user presses Enter in the console window
         meterApp.StartCaptureAndOverlay();
         Console.ReadLine();
         meterApp.StopCaptureAndOverlay();
