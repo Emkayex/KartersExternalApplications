d1f4ee8669170fd46d43c36b1dd3e0da662d2b5e
Emkayex <53287857+Emkayex@users.noreply.github.com>
2024-05-20 18:55:13 -0500

    Updated Build Script to Make ZIP File

--- PublishExe.py
+++ PublishExe.py
@@ -1,9 +1,14 @@
 from argparse import ArgumentParser
 from os import system as os_system
+from pathlib import Path
 from re import match as re_match
+from shutil import make_archive
+from tempfile import TemporaryDirectory
 
 
 SEMVER_REGEX = r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+CS_PROJ = Path(__file__).parent.joinpath("CNKStyleBoostBar", "CNKStyleBoostBar.csproj")
+RID = "win-x64" # Only AMD64 Windows will be supported officially
 
 
 def main() -> None:
@@ -18,17 +23,22 @@ def main() -> None:
     if match is None:
         raise RuntimeError(f"Invalid version number: {version}")
 
-    cmd = " ".join([
-        "dotnet",
-        "publish",
-        "-c",
-        "Release",
-        "-r",
-        "win-x64", # Only AMD64 Windows will be supported officially
-        f'/p:Version="{version}"'
-    ])
+    with TemporaryDirectory() as td:
+        cmd = " ".join([
+            "dotnet",
+            "publish",
+            "-c",
+            "Release",
+            "-r",
+            RID,
+            "-o",
+            f'"{td}"',
+            f'/p:Version="{version}"',
+            f'"{CS_PROJ}"'
+        ])
 
-    os_system(cmd)
+        os_system(cmd)
+        make_archive(f"{CS_PROJ.stem}-{RID}", "zip", td)
 
 
 if __name__ == "__main__":
